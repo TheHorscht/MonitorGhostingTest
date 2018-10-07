33fa611d8b59421025b149b573dacc089edece50
The Horscht <realhorscht@gmail.com>
2018-10-07 20:41:26 +0200

    fixed root public path

--- config/index.js
+++ config/index.js
@@ -9,7 +9,7 @@ module.exports = {
 
     // Paths
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: '/MonitorGhostingTest',
     proxyTable: {},
 
     // Various Dev Server settings
